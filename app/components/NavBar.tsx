500d4012e4b91c6c360e3ec34ade0dc33d357b22
ariuka-09 <ariuntuguldur3@gmail.com>
2025-12-01 10:43:09 +0800

    fixed, it summarizes an article, adds history

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,16 +1,20 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 export const NavBar = () => {
   return (
     <div className="h-14 flex justify-between w-full items-center  border-b">
       <p className="text-[24px] font-semibold ">Quiz app</p>
-      <Avatar>
+      {/* <Avatar>
         <AvatarImage
           className="w-10 h-10 rounded-[50%] "
           src="https://github.com/shadcn.png"
         />
         <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
+      </Avatar> */}
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@
 import Image from "next/image";
 import { axiosInstance } from "./lib/utils";
 import { useEffect, useState } from "react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 export default function Home() {
   const [employees, setEmployees] = useState([]);
@@ -20,15 +27,17 @@ export default function Home() {
   }, []);
   return (
     <div>
-      {employees.map((e, i) => {
-        return (
-          <div key={i}>
-            <div>{e.firstname}</div>
-            <div>{e.lastname}</div>
-            <div>{e.age}</div>
-          </div>
-        );
-      })}
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton>
+          <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
