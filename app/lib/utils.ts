512a544140ef8bc89ee9cf48f6e62e57719ef320
ariuntuguldur3@gmail.com <ariuntuguldur3@gmail.com>
2026-01-05 23:39:35 +0800

    fix: upgrade Next.js to patched version

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "/api",
   // headers: {
   //   Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_TOKEN}`,
   // },
