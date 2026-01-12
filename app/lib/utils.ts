1567ae1126a18979de1699ccd3db79e7d96544b0
ariuka-09 <ariuntuguldur3@gmail.com>
2026-01-12 10:02:34 +0800

    fixed axiosInstance base url

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: "https://quiz-app-one-flax-73.vercel.app/api",
   // headers: {
   //   Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_TOKEN}`,
   // },
